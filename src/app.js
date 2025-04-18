1cb942a681455009915906dffbe954bb3e35bbd8
GabrielOque <oquendodev@gmail.com>
2025-04-18 13:46:35 -0500

    feat: configure CORS middleware with specific origins for enhanced security

--- src/app.js
+++ src/app.js
@@ -10,7 +10,18 @@ const app = express();
 //middleware
 app.use(express.json());
 app.use(fileUpload());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "https://daily-planner-back.onrender.com",
+      "https://daily-planner-back-1.onrender.com",
+      "http://localhost:4000",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use("/api/v1/user", usersRoutes);
 app.use("/api/v1/code", codesRoutes);
 
