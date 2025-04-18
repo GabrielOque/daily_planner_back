e5444d4867c296c45164c00b5ef57c0f684a24fb
GabrielOque <oquendodev@gmail.com>
2025-04-18 13:12:27 -0500

    feat: update routing structure to separate user and code endpoints

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import fileUpload from "express-fileupload";
 import cors from "cors";
 
 import usersRoutes from "./routes/users.routes.js";
+import codesRoutes from "./routes/codes.routes.js";
 
 const app = express();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(fileUpload());
 app.use(cors());
-app.use("/api/v1", usersRoutes);
+app.use("/api/v1/user", usersRoutes);
+app.use("/api/v1/code", codesRoutes);
 
 export default app;
